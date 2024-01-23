b6462ed2af3790aee54c3a15a6129c820664e977
Carlos Paniagua <cpaniaguam@gmail.com>
2024-01-23 16:57:57 -0500

    test: extract input data files

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shutil
 import tarfile
 from pathlib import Path
 
@@ -8,22 +9,67 @@ def checkpaths(paths):
     return all([os.path.isfile(pth) for pth in paths])
 
 
+def cleanup_directory(directory_path):
+    if os.path.exists(directory_path):
+        shutil.rmtree(directory_path)
+
+
+def extract_specific_files(tar_path, files_to_extract, output_dir):
+    """
+    Extract a specific file from a tarball to a specific directory keeping the same file structure as in the tarball.
+
+    Parameters
+    ----------
+    tar_path : str
+        Path to the tarball file.
+    file_to_extract : str
+        Path to the file to extract from the tarball.
+    output_dir : str
+        Path to the directory to extract the file to.
+    """
+
+    # make sure the output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+    for file_to_extract in files_to_extract:
+        with tarfile.open(tar_path, "r:gz") as tar:
+            extract_to = os.path.join(output_dir, file_to_extract)
+            os.makedirs(os.path.dirname(extract_to), exist_ok=True)
+            print(file_to_extract)
+            with open(extract_to, "wb") as out_f:
+                in_f = tar.extractfile(str(file_to_extract))
+                out_f.write(in_f.read())
+
+
 def makepathlist(dir, files):
     return [Path(dir, f) for f in files]
 
 
-def setup_module(module):
+def setup_module():
     # Extract the plots.tar.gz and work.tar.gz files in the tests directory to the home directory
     tarballs = ["plots.tar.gz", "work.tar.gz"]
-    for tarball in tarballs:
-        tar = tarfile.open(Path("tests", tarball))
-        tar.extractall()
-        tar.close()
+    plotstb, worktb = tarballs
+
+    tarpath = Path("tests", worktb)
+    for i, data in enumerate([data2, data3, data4], 2):
+        extract_specific_files(tarpath, data, f"work{i}")
 
 
-def teardown_module(module):
-    # Remove the plots/ and work/ directories
-    os.system("rm -r plots work")
+# for tarball in tarballs:
+#     tar = tarfile.open(Path("tests", tarball))
+#     tar.extractall()
+#     tar.close()
+
+
+# def teardown_module(module):
+#     # Remove the plots/ and work/ directories
+#     cleanup_directory("work2")
+#     # os.system("rm -r plots work*")
+
+
+# def teardown_module_other():
+#     # Remove the plots/ and work/ directories
+#     cleanup_directory("work2")
+#     # os.system("rm -r plots work*")
 
 
 script1 = [
@@ -48,6 +94,7 @@ script2 = [
     "SH_testSLsinglefile2",
     "True",
 ]
+data2 = ["work/SH_testSLsinglefile2/B01_regrid/SH_20190502_05180312_B01_binned.h5"]
 paths2 = [
     "work/SH_testSLsinglefile2/B02_spectra/B02_SH_20190502_05180312_params.h5",
     "work/SH_testSLsinglefile2/B02_spectra/B02_SH_20190502_05180312_gFT_x.nc",
@@ -73,6 +120,14 @@ _paths3 = [
     "B03_spectra/B03_freq_reconst_x75.pdf",
     "B03_success.json",
 ]
+data3 = [
+    Path("work/SH_testSLsinglefile2/B02_spectra") / p
+    for p in [
+        "B02_SH_20190502_05180312_FFT.nc",
+        "B02_SH_20190502_05180312_gFT_k.nc",
+        "B02_SH_20190502_05180312_gFT_x.nc",
+    ]
+]
 paths3 = makepathlist(_dir, _paths3)
 
 script4 = [
@@ -88,6 +143,7 @@ paths4 = [
     "work/SH_testSLsinglefile2/A02_prior/A02_SH_20190502_05180312.h5",
     "work/SH_testSLsinglefile2/A02_prior/A02_SH_20190502_05180312_hindcast_success.json",
 ]
+data4 = ["work/SH_testSLsinglefile2/B01_regrid/SH_20190502_05180312_B01_binned.h5"]
 
 script5 = [
     "python",
@@ -106,31 +162,46 @@ paths5 = [
 ]
 
 
-def test_directories_and_files_step1():
-    # Step 1: B01_SL_load_single_file.py ~ 9 minutes
-    subprocess.run(script1, check=True)
-    assert checkpaths(paths1)
+# def test_directories_and_files_step1():
+#     # Step 1: B01_SL_load_single_file.py ~ 9 minutes
+#     subprocess.run(script1, check=True)
+#     assert checkpaths(paths1)
 
 
 def test_directories_and_files_step2():
     # Step 2: B02_make_spectra_gFT.py ~ 2 min
-    subprocess.run(script2, check=True)
-    assert checkpaths(paths2)
+
+    # subprocess.run(script2, check=True)
+    # Check "work2/work/SH_testSLsinglefile2/B01_regrid/SH_20190502_05180312_B01_binned.h5" exists
+    assert all([os.path.isfile(Path("work2", p)) for p in data2])
+
+    # assert checkpaths(paths2)
 
 
 def test_directories_and_files_step3():
-    # Step 3: B03_plot_spectra_ov.py ~ 11 sec
-    subprocess.run(script3, check=True)
-    assert checkpaths(paths3)
+    #     # Step 3: B03_plot_spectra_ov.py ~ 11 sec
+    #     subprocess.run(script3, check=True)
+    assert all([os.path.isfile(Path("work3", p)) for p in data3])
 
 
 def test_directories_and_files_step4():
-    # Step 4: A02c_IOWAGA_thredds_prior.py ~ 23 sec
-    subprocess.run(script4, check=True)
-    assert checkpaths(paths4)
+    #     # Step 4: A02c_IOWAGA_thredds_prior.py ~ 23 sec
+    #     subprocess.run(script4, check=True)
+    assert all([os.path.isfile(Path("work4", p)) for p in data4])
 
 
-def test_directories_and_files_step5():
-    # Step 5: B04_angle.py ~ 9 min
-    subprocess.run(script5, check=True)
-    assert checkpaths(paths5)
+# def test_directories_and_files_step5():
+#     # Step 5: B04_angle.py ~ 9 min
+#     subprocess.run(script5, check=True)
+#     assert checkpaths(paths5)
+
+
+def test_foo():
+    assert True
+
+
+if __name__ == "__main__":
+    setup_module()
+    test_directories_and_files_step2()
+    test_directories_and_files_step3()
+    test_directories_and_files_step4()
