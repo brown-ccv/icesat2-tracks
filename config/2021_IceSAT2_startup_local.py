b21d78796070eec8c55b61ceb7956491d793170a
Carlos Paniagua <cpaniaguam@gmail.com>
2024-01-25 15:11:04 -0500

    chore: clean up 2021_IceSAT2_startup_local.py

--- config/2021_IceSAT2_startup_local.py
+++ config/2021_IceSAT2_startup_local.py
@@ -1,29 +1,13 @@
 import os
-#os.environ["DISPLAY"] = "localhost:10.0"
-# 14, 16,  work
-#standart libraries:
-import numpy as np
-import matplotlib
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import pandas as pd
+import sys
 
-from matplotlib.gridspec import GridSpec
 import string
+from icesat2_tracks.local_modules import m_colormanager_ph3 as M_color
 
 import xarray as xr
-#xr.set_options(display_width=80, display_style='text')
 xr.set_options(display_style='text')
 
-import sys
-import imp
-
-import string
-
-# my own libraries:
-#import m_general as M
-
-#import AA_plot_base as AA
 def json_load(name, path, verbose=False):
     import json
     full_name= (os.path.join(path,name+ '.json'))
@@ -40,11 +24,10 @@ mconfig=json_load('config_local','/Users/Shared/Projects/2021_IceSAT2_tracks/con
 sys.path.append(mconfig['paths']['local_script'])
 sys.path.append(mconfig['paths']['local_script'] +'/ICEsat2_SI_tools/')
 
-import m_colormanager_ph3 as M_color
-import m_tools_ph3 as MT
-import m_general_ph3 as M
+
 
 #load colorscheme
+# The color method produces output to stdout; suppresed in the class def. CP
 col=M_color.color(path=mconfig['paths']['analysis']+'../config/', name='color_def')
 
 
@@ -56,16 +39,7 @@ fig_sizes = mconfig['fig_sizes']['AMS']
 SMALL_SIZE = 8
 MEDIUM_SIZE = 10
 BIGGER_SIZE = 12
-#csfont = {'fontname':'Comic Sans MS'}
 legend_properties = {'weight':'bold'}
-#font.family: sans-serif
-#font.sans-serif: Helvetica Neue
-
-#import matplotlib.font_manager as font_manager
-#font_dirs = ['/home/mhell/HelveticaNeue/', ]
-#font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
-#font_list = font_manager.createFontList(font_files)
-#font_manager.fontManager.ttflist.extend(font_list)
 
 plt.rc('font', size=SMALL_SIZE, serif='Helvetica Neue', weight='normal')          # controls default text sizes
 #plt.rc('font', size=SMALL_SIZE, serif='DejaVu Sans', weight='light')
@@ -75,19 +49,11 @@ plt.rc('axes', labelsize=SMALL_SIZE, labelweight='normal') #, family='bold')    
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE, frameon=False)    # legend fontsize
-plt.rc('figure', titlesize=MEDIUM_SIZE, titleweight='bold', autolayout=True) #, family='bold')  # fontsize of the figure title
-
-#figure.autolayout : False
-#matplotlib.rcParams['pdf.fonttype'] = 42
-#matplotlib.rcParams['ps.fonttype'] = 42
-
+plt.rc('figure', titlesize=MEDIUM_SIZE, titleweight='bold', autolayout=True)
 
 plt.rc('path', simplify=True)
 
 plt.rcParams['figure.figsize'] = (10, 8)#(20.0, 10.0) #inline
-#plt.rcParams['pcolor.shading'] = 'auto'
-#rcParams['pcolor.shading'] = 'auto'
-#plt.rc('pcolor', shading = 'auto')
 
 ### TICKS
 # see http://matplotlib.org/api/axis_api.html#matplotlib.axis.Tick
@@ -125,50 +91,13 @@ plt.rcParams['figure.figsize'] = (10, 8)#(20.0, 10.0) #inline
 #ytick.minor.left     : True   # draw y axis left minor ticks
 #ytick.minor.right    : True   # draw y axis right minor ticks
 
-
 plt.rc('xtick.major', size= 4, width=1 )
 plt.rc('ytick.major', size= 3.8, width=1 )
 
-#axes.facecolor      : white   # axes background color
-#axes.edgecolor      : black   # axes edge color
-#axes.linewidth      : 0.8     # edge linewidth
-#axes.grid           : False   # display grid or not
-#axes.titlesize      : large   # fontsize of the axes title
-#axes.titlepad       : 6.0     # pad between axes and title in points
-#axes.labelsize      : medium  # fontsize of the x any y labels
-#axes.labelpad       : 4.0     # space between label and axis
-#axes.labelweight    : normal  # weight of the x and y labels
-#axes.labelcolor     : black
-
 plt.rc('axes', labelsize= MEDIUM_SIZE, labelweight='normal')
-
-
-
-
-# axes.spines.left   : True   # display axis spines
-# axes.spines.bottom : True
-# axes.spines.top    : True
-# axes.spines.right  : True
 plt.rc('axes.spines', top= False, right=False )
 
-
-
-def font_for_print():
-
-    SMALL_SIZE = 6
-    MEDIUM_SIZE = 8
-    BIGGER_SIZE = 10
-    #csfont = {'fontname':'Comic Sans MS'}
-    legend_properties = {'weight':'bold'}
-    #font.family: sans-serif
-    #font.sans-serif: Helvetica Neue
-
-    #import matplotlib.font_manager as font_manager
-    #font_dirs = ['/home/mhell/HelveticaNeue/', ]
-    #font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
-    #font_list = font_manager.createFontList(font_files)
-    #font_manager.fontManager.ttflist.extend(font_list)
-
+def font_for_print(SMALL_SIZE = 6, MEDIUM_SIZE = 8, BIGGER_SIZE = 10):
     plt.rc('font', size=SMALL_SIZE, serif='Helvetica Neue', weight='normal')          # controls default text sizes
     #plt.rc('font', size=SMALL_SIZE, serif='DejaVu Sans', weight='light')
     plt.rc('text', usetex='false')
@@ -177,35 +106,11 @@ def font_for_print():
     plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
     plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
     plt.rc('legend', fontsize=SMALL_SIZE, frameon=False)    # legend fontsize
-    plt.rc('figure', titlesize=MEDIUM_SIZE, titleweight='bold', autolayout=True) #, family='bold')  # fontsize of the figure title
-
-    #figure.autolayout : False
-    #matplotlib.rcParams['pdf.fonttype'] = 42
-    #matplotlib.rcParams['ps.fonttype'] = 42
-
-
-    #plt.rc('xtick.major', size= 4, width=1 )
-    #plt.rc('ytick.major', size= 3.8, width=1 )
-
+    plt.rc('figure', titlesize=MEDIUM_SIZE, titleweight='bold', autolayout=True)
 
     plt.rc('axes', labelsize= SMALL_SIZE, labelweight='normal')
 
-def font_for_pres():
-
-    SMALL_SIZE = 10
-    MEDIUM_SIZE = 12
-    BIGGER_SIZE = 14
-    #csfont = {'fontname':'Comic Sans MS'}
-    legend_properties = {'weight':'bold'}
-    #font.family: sans-serif
-    #font.sans-serif: Helvetica Neue
-
-    #import matplotlib.font_manager as font_manager
-    #font_dirs = ['/home/mhell/HelveticaNeue/', ]
-    #font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
-    #font_list = font_manager.createFontList(font_files)
-    #font_manager.fontManager.ttflist.extend(font_list)
-
+def font_for_pres(SMALL_SIZE = 10, MEDIUM_SIZE = 12, BIGGER_SIZE = 14):
     plt.rc('font', size=SMALL_SIZE, serif='Helvetica Neue', weight='normal')          # controls default text sizes
     #plt.rc('font', size=SMALL_SIZE, serif='DejaVu Sans', weight='light')
     plt.rc('text', usetex='false')
@@ -214,20 +119,6 @@ def font_for_pres():
     plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
     plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
     plt.rc('legend', fontsize=SMALL_SIZE, frameon=False)    # legend fontsize
-    plt.rc('figure', titlesize=MEDIUM_SIZE, titleweight='bold', autolayout=True) #, family='bold')  # fontsize of the figure title
-
-    #figure.autolayout : False
-    #matplotlib.rcParams['pdf.fonttype'] = 42
-    #matplotlib.rcParams['ps.fonttype'] = 42
-
-
-    #plt.rc('xtick.major', size= 4, width=1 )
-    #plt.rc('ytick.major', size= 3.8, width=1 )
-
+    plt.rc('figure', titlesize=MEDIUM_SIZE, titleweight='bold', autolayout=True)
 
     plt.rc('axes', labelsize= SMALL_SIZE, labelweight='normal')
-
-
-
-# add project depenent libraries
-#sys.path.append(config['paths']['local_script'])
