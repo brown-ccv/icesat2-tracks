29a039997bc6bef4407d12c1a17e6ce221a9364e
John Gerrard Holland <john_holland1@brown.edu>
2024-02-29 10:39:13 -0500

    feat: aadd basic logging to b01 when run as __main__

--- src/icesat2_tracks/analysis_db/B01_SL_load_single_file.py
+++ src/icesat2_tracks/analysis_db/B01_SL_load_single_file.py
@@ -282,4 +282,5 @@ def run_B01_SL_load_single_file(
 load_file_app = makeapp(run_B01_SL_load_single_file, name="load-file")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.NOTSET)
     load_file_app()
